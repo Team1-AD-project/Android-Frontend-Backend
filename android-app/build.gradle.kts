812b7bc3860b601bc36c099a0adbe2d5f89d8a8c
qqawer <1678627439@qq.com>
2026-02-05 13:26:48 +0800

    update plugins

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.6" apply false
 }
